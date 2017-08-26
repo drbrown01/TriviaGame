df9a68704a2235e042627ddfe219471c19b8266c
Dan <drbrown01@yahoo.com>
2017-08-25 20:54:49 -0500

    added api and html

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,21 +8,37 @@ window.onload = function() {
   $("#start").on("click", stopwatch.start);
 };
 
-var qz = "https://opentdb.com/api.php?amount=1";
+var qz = "https://opentdb.com/api.php?amount=1&type=multiple";
+
 
 $.ajax({
   url: qz,
   method: "GET"
 }).done(function(response) {
+var question = [];
+var len = response.results.length;
 
-  console.log(response);
+// var answers =
+  // console.log(question);
+  for(var i=0;i< len; i++){
+    question.push(response.results[i].question);
+    console.log(question);
+  $(".questions").html(question);
+  $(".answers").html( response.results[0].correct_answer);
+   $(".answers").html("<br>" + response.results[0].incorrect_answers);
+
+  //  $(".answers").click(function()
+  //   $(".answers").html(response.results[0].incorrect_answers[1])
+  //     $(".answers").html(response.results[0].incorrect_answers[2])
+}
 })
 var unAnswered = 0
 var numWrong = 0
+
 var numRight = 0
 // var countDown = setTimeout(funciton(), 30000)
 var clockRunning = false;
-var timeLeft = setTimeout(function(){alert("GameStart")}, 30000);
+// var timeLeft = setTimeout(function(){alert("GameStart")}, 30000);
 var stopwatch = {
   time: 0,
   reset: function(){
@@ -38,12 +54,12 @@ var stopwatch = {
 
 
 
-    $( window ).on( "load", function() {
-        console.log( "window loaded" );
-    });
-
-    $("#timeLeft").text("<div>" + timeLeft + "</div>")
-    $("start").html("<div>" + qz +"<div>")
+    // $( window ).on( "load", function() {
+    //     console.log( "window loaded" );
+    // });
+    //
+    // // $("#timeLeft").text("<div>" + timeLeft + "</div>")
+    // $("start").html("<div>" + qz +"<div>")
 
 
 //time counter
